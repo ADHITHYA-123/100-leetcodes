ae2293fee1a283611f54c514603a8a589fac070f
Adhithya Shanil <adhithyasonu@gmail.com>
2024-07-11 19:09:59 +0530

    Kth largest element

---
+++ Leetcode Solutions/Python solutions/Kth_larrgest_element.py
@@ -0,0 +1,22 @@
+
+def quickselect(arr,k,l,r):
+    pivot,p=arr[r],l
+    for i in range(l,r):
+        if arr[i]<=pivot:
+            arr[i],arr[p]=arr[p],arr[i]
+            p+=1
+    arr[p],arr[r]=arr[r],arr[p]
+    if p>k:
+        quickselect(arr,k,l,p-1)
+    elif p<k:
+        quickselect(arr,k,p+1,r)
+    else:
+        print(f"The kth{k} largest element is : {arr[p]}")
+    
+size=int(input('Enter the size of the array: '))
+arr=[]
+for i in range(size):
+    arr.append(int(input(f'Enter the element {i+1} : ')))
+k=int(input('Enter the value of k: '))
+k=size-k
+quickselect(arr,k,0,size-1)
